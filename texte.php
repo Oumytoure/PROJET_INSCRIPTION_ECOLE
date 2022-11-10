a641ee1b9bca86abbc205783b162398016570724
oumy <toureoumy12@gmail.com>
2022-11-10 14:44:56 +0000

    test

---
+++ texte.php
@@ -0,0 +1,5 @@
+DOCUMENTATION DU CITE
+Par Défaut on arrive a la page de connection l'utilisateur, s'il entre un mail ou un mot de passe qui n'est pas correcte on le dit d'entrer un 
+ bon mail ou mot de passe ;si les champs sont vide aussi on l'autorise pas à se connecter. s'il entre un mot de passe ou nun mail correcte.Il se
+ connecte et entre dans la page inscription ;si il n'est pas inscrite et veut
+ 
